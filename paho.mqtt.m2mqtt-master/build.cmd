c56fa7b435ccf05e61cf5b5eb173438454ee0d1e
Stefan Roßmann <info@rossmann-engineering.de>
2018-09-20 22:10:15 +0200

    MQTT

--- paho.mqtt.m2mqtt-master/build.cmd
+++
@@ -1,6 +0,0 @@
-
-MSBuild.exe M2Mqtt.sln /p:Configuration=Release
-
-IF NOT EXIST ".\Build\Packages" MKDIR ".\Build\Packages"
-
-.\Tools\NuGet\NuGet.exe pack M2Mqtt.nuspec -OutputDirectory ".\Build\Packages"
