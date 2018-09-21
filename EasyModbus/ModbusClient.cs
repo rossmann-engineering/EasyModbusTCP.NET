db04194dc50046fd2cb5125af02a6e37c19e5a0f
rossmann-engineering <info@rossmann-engineering.de>
2018-09-21 07:36:18 +0200

    V5.5

--- EasyModbus/ModbusClient.cs
+++ EasyModbus/ModbusClient.cs
@@ -3,7 +3,7 @@
  * 
  * This program is free software: you can redistribute it and/or modify  
  * it under the terms of the GNU General Public License as published by  
- * the Free Software Foundation, version 3.
+ * the Free Software Foundation, version 3. 
  *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of 
