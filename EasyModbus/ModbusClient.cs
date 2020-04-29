28318fcc25ea3a0bb2d9413bce4ffb50bf9aecd5
Stefan Rossmann <info@rossmann-engineering.de>
2020-04-29 07:26:49 +0200

    Updated License Information

--- EasyModbus/ModbusClient.cs
+++ EasyModbus/ModbusClient.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2018 Stefan Roßmann.
+ * Copyright (c) 2018-2020 Stefan Roßmann.
  * 
  * This program is free software: you can redistribute it and/or modify  
  * it under the terms of the GNU General Public License as published by  
@@ -13,8 +13,6 @@
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * http://www.rossmann-engineering.de
- * Datum: 16.06.2011
- * Zeit: 21:35
  */
 using System;
 using System.Net.Sockets;
--- EasyModbusServerSimulator/Program.cs
+++ EasyModbusServerSimulator/Program.cs
@@ -1,6 +1,18 @@
 ﻿/*
- * Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
- * User: www.rossmann-engineering.de 
+ * Copyright (c) 2018-2020 Stefan Roßmann.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3. 
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * http://www.rossmann-engineering.de
  */
 using System;
 using System.Windows.Forms;
