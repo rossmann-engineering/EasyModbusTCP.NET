acc99eba646a8e2da2d236df365f3083de8af993
georgemakrakis <georgemakrakis88@gmail.com>
2021-08-26 20:58:04 -0600

    Changed default port to 802 by MODBUS/TCP Security Protocol Spec

--- EasySecureModbus_Demo/Program.cs
+++ EasySecureModbus_Demo/Program.cs
@@ -11,11 +11,11 @@ namespace EasuySecureModbus_Demo
     {
         static void Main(string[] args) // For now we pass only the password, later can be cert path as well
         {
-            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 502, "..\\..\\certs\\client.pfx", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
-            ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 502);    //Ip-Address and Port of Modbus-TCP-Server
-            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 502, null, args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
-            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 502, "", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
-            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 502, "..\\..\\certs\\something.pfx", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
+            ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 802, "..\\..\\certs\\client.pfx", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
+            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 802);    //Ip-Address and Port of Modbus-TCP-Server
+            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 802, null, args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
+            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 802, "", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
+            //ModbusSecureClient modbusClient = new ModbusSecureClient("127.0.0.1", 802, "..\\..\\certs\\something.pfx", args[0], true);    //Ip-Address and Port of Modbus-TCP-Server
             
             modbusClient.LogFileFilename = "..\\..\\logs\\ClientLogs.txt";
             modbusClient.Connect();                                                    //Connect to Server
