c55185c4f0625e64b1cac48cf0f4bc5c14b1df5e
georgemakrakis <georgemakrakis88@gmail.com>
2022-02-03 21:17:38 -0700

    In testing, used reflection to test ReadCoils startingAddress is out of range.

---
+++ EasyModbus_SecureTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EasyModbus_SecureTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyModbus_SecureTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1640d09e-d68e-46ce-abf2-8623b0370802")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
